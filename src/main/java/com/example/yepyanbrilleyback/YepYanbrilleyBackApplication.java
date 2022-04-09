59d38f32c6d58d08c9a5bd0cfbb477043eec519d
Brilley <20120251@bjtu.edu.cn>
2022-04-09 23:56:10 +0800

    first commit

---
+++ src/main/java/com/example/yepyanbrilleyback/YepYanbrilleyBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.yepyanbrilleyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YepYanbrilleyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YepYanbrilleyBackApplication.class, args);
+    }
+
+}
